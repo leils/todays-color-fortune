dce685eef47f044140f9bc2bc95b7d6e1b87a284
Leia Chang <leia@squareup.com>
2018-10-26 17:37:09 -0700

    Updates

---
+++ precache-manifest.1013463e75de30d410b62dc61f2b939c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2256892b24586db08e89",
+    "url": "/todays-color-fortune/static/js/runtime~main.2256892b.js"
+  },
+  {
+    "revision": "cbd40c66964753c26bc9",
+    "url": "/todays-color-fortune/static/js/main.cbd40c66.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/todays-color-fortune/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "cbd40c66964753c26bc9",
+    "url": "/todays-color-fortune/static/css/main.2476c46d.chunk.css"
+  },
+  {
+    "revision": "e40288e05957f032517bab4602b9d673",
+    "url": "/todays-color-fortune/index.html"
+  }
+];
