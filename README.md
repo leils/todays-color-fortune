05d52259ad50e178b7dd671c11ed7f15f1f4e6b3
Leia <e.leiachang@gmail.com>
2018-11-26 18:34:51 -0800

    Added hyperlinks to color hex, footer

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+And hosted on GitHub Pages using [react-gh-pages](https://github.com/gitname/react-gh-pages).
 
 Below you will find some information on how to perform common tasks.<br>
 You can find the most recent version of this guide [here](https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md).
--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ function getRandomColor() {
   return color;
 }
 
+var randcolor1 = getRandomColor()
+var randcolor2 = getRandomColor()
+var randcolor3 = getRandomColor()
+var randcolor4 = getRandomColor()
+var randcolor5 = getRandomColor()
+
 class App extends Component {
   render() {
     return (
@@ -17,21 +23,41 @@ class App extends Component {
         <header className="App-header">
           <p> Today's color fortune is: </p>
           <div class="row">
-            <div class="column" style={{backgroundColor: getRandomColor()}} >
-            </div>
+            <a href={`https://www.color-hex.com/color/${randcolor1.slice(1)}` } target="blank">
+              <div class="column" style={{backgroundColor: randcolor1}} >
+                {randcolor1}
+              </div>
+            </a>
 
-            <div class="column" style={{backgroundColor: getRandomColor()}} >
-            </div>
+            <a href={`https://www.color-hex.com/color/${randcolor2.slice(1)}`} target="blank">
+              <div class="column" style={{backgroundColor: randcolor2}} >
+                {randcolor2}
+              </div>
+            </a>
 
-            <div class="column" style={{backgroundColor: getRandomColor()}} >
-            </div>
+            <a href={`https://www.color-hex.com/color/${randcolor3.slice(1)}`} target="blank">
+              <div class="column" style={{backgroundColor: randcolor3}} >
+                {randcolor3}
+              </div>
+            </a>
 
-            <div class="column" style={{backgroundColor: getRandomColor()}} >
-            </div>
+            <a href={`https://www.color-hex.com/color/${randcolor4.slice(1)}`} target="blank">
+              <div class="column" style={{backgroundColor: randcolor4}} >
+                {randcolor4}
+              </div>
+            </a>
 
-            <div class="column" style={{backgroundColor: getRandomColor()}} >
-            </div>
+            <a href={`https://www.color-hex.com/color/${randcolor5.slice(1)}`} target="blank">
+              <div class="column" style={{backgroundColor: randcolor5}} >
+                {randcolor5}
+              </div>
+            </a>
           </div>
+
+          <div class="footer">
+            Experiment by <a href="http://leiac.me" target="blank">Leia Chang</a>.
+          </div>
+
         </header>
       </div>
     );
